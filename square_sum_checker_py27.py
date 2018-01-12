fec3ee5103bede47fd72cd031c47015be50a98ad
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2018-01-12 20:06:21 +0000

    added checker

---
+++ square_sum_checker_py27.py
@@ -0,0 +1,22 @@
+# Use this program to check your list-style Square-Sum Hamiltonian Paths!
+
+# This program works in python 2 ONLY
+
+arr = input()
+numbers = set([i for i in range(1, len(arr) + 1)])
+
+last = None
+for n in arr:
+    if last is not None:
+        if (n+last) ** 0.5 % 1 != 0:
+            print("FAIL, Values do not add up to square number")
+            break
+    try:
+        numbers.remove(n)
+    except KeyError:
+        print(n)
+        print("FAIL, Incorrect number of values")
+        break
+    last = n
+else:
+    print("Success!")
